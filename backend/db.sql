8197fa6d208cf7368d89dfa5a3eb4b1f70eba4eb
gunavc <97451210+gunavc@users.noreply.github.com>
2023-11-10 12:12:33 +0530

    Modified Database Structure

--- backend/db.sql
+++ backend/db.sql
@@ -12,47 +12,52 @@ USE toll_booth;
 -- FLUSH PRIVILEGES;
 
 
-CREATE TABLE usr_info(
+CREATE TABLE login_user_info(
     usrid INT AUTO_INCREMENT PRIMARY KEY,
     f_name VARCHAR(25),
     minit VARCHAR(5),
     l_name VARCHAR(25),
     username VARCHAR(25) UNIQUE,
     hashed_pass VARCHAR(255),
-    auth_level ENUM('0','1') DEFAULT '1',
-    CONSTRAINT uname_pass UNIQUE (username,hashed_pass)
+    auth_level ENUM('admin','operator'),
+    CONSTRAINT uname_pass UNIQUE (username,hashed_pass),
+    CONSTRAINT usrid_fk FOREIGN KEY (usrid) REFERENCES employee_info(usrid)
 );
 
 
-CREATE TABLE usr_info1 (
+CREATE TABLE employee_info (
     usrid INT AUTO_INCREMENT PRIMARY KEY,
     f_name VARCHAR(25),
     minit VARCHAR(5),
     l_name VARCHAR(25),
-    username VARCHAR(25) UNIQUE,
-    hashed_pass VARCHAR(255),
-    auth_level ENUM('0','1') DEFAULT '1',
+    -- username VARCHAR(25) UNIQUE,
+    -- hashed_pass VARCHAR(255),
+    auth_level ENUM('admin','operator', 'support') DEFAULT 'support',
     address VARCHAR(255), -- New column
-    date_of_joining DATE, -- New column
-    CONSTRAINT uname_pass UNIQUE (username, hashed_pass)
+    date_of_joining DATE -- New column
 );
 
 -- Step 4: Reinsert the values into the new table
-INSERT INTO usr_info1 (f_name, l_name, minit, username, hashed_pass, auth_level, address, date_of_joining) VALUES
-('John', 'Doe', 'A', 'johndoe1', 'pass123', '1', '123 Main St', '2023-01-01'),
-('Alice', 'Smith', 'B', 'alice.smith', 'abc456', '1', '456 Oak St', '2023-02-15'),
-('Bob', 'Johnson', 'C', 'bobj', 'bob789', '0', '789 Pine St', '2023-03-10'),
-('Emily', 'Davis', 'D', 'emily_d', 'emily123', '1', '987 Elm St', '2023-04-05'),
-('Michael', 'Wilson', 'E', 'mike.w', 'mike456', '1', '654 Birch St', '2023-05-20'),
-('Sarah', 'Brown', 'F', 'sarah_b', 'sarah789', '1', '321 Cedar St', '2023-06-15'),
-('David', 'Lee', 'G', 'davidl', 'david123', '1', '876 Pine St', '2023-07-01'),
-('Laura', 'Clark', 'H', 'laura.c', 'laura456', '1', '543 Maple St', '2023-08-12'),
-('James', 'Anderson', 'I', 'james123', 'pass789', '0', '234 Oak St', '2023-09-28'),
-('Sophia', 'Martinez', 'J', 'sophiam', 'sophia123', '1', '789 Elm St', '2023-10-03'),
-('Admin','Admin','','admin','abc','0', '555 Admin St', '2023-11-09'),
-('Peter','Parker','P','ppparker','1234567890','0', '123 Web St', '2023-12-25'),
-('James','May','','jamesmay','captainslow','1', '999 Slow St', '2024-01-10'),
-('Richard','Hammond','','richardham','hamster','1', '777 Hamster St', '2024-02-14');
+INSERT INTO employee_info(f_name, l_name, minit, username, hashed_pass, auth_level, address, date_of_joining) VALUES
+('John', 'Doe', 'A', '1', '123 Main St', '2023-01-01'),
+('Alice', 'Smith', 'B', '1', '456 Oak St', '2023-02-15'),
+('Bob', 'Johnson', 'C', '0', '789 Pine St', '2023-03-10'),
+('Emily', 'Davis', 'D', '1', '987 Elm St', '2023-04-05'),
+('Michael', 'Wilson', 'E', '1', '654 Birch St', '2023-05-20'),
+('Sarah', 'Brown', 'F', '1', '321 Cedar St', '2023-06-15'),
+('David', 'Lee', 'G', '1', '876 Pine St', '2023-07-01'),
+('Laura', 'Clark', 'H', '1', '543 Maple St', '2023-08-12'),
+('James', 'Anderson', 'I', '0', '234 Oak St', '2023-09-28'),
+('Sophia', 'Martinez', 'J', '1', '789 Elm St', '2023-10-03'),
+('Admin','Admin','','0', '555 Admin St', '2023-11-09'),
+('Peter','Parker','P','0', '123 Web St', '2023-12-25'),
+('James','May','', '1', '999 Slow St', '2024-01-10'),
+('Richard','Hammond','','1', '777 Hamster St', '2024-02-14');
+
+INSERT INTO login_user_info(f_name, l_name, minit, username, hashed_pass, auth_level) VALUES
+('John', 'Doe', 'A', 'johndoe1', 'pass123', '1'),
+('Alice', 'Smith', 'B', 'alice.smith', 'abc456', '1'),
+('Bob', 'Johnson', 'C', 'bobj', 'bob789', '0');
 
 CREATE TABLE cost_matrix
 (   
